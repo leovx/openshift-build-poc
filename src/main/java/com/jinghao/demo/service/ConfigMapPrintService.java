af2c32a1521e2dfdfaaeb03a8a14a622d8d880df
Jinghao Song <leovx@qq.com>
2022-01-24 21:57:52 +0800

    poc

---
+++ src/main/java/com/jinghao/demo/service/ConfigMapPrintService.java
@@ -0,0 +1,20 @@
+package com.jinghao.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ConfigMapPrintService {
+
+    @Value("${poc_var:abc}")
+    private String pocVar;
+
+    @Scheduled(fixedRate = 1000)
+    public void printVar() {
+        log.info(pocVar);
+    }
+
+}
